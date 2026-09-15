4a315bad0d095de913b8b4962c0e5039b5ee283d
Bryson Grant <Bryson.grant@stu.ocsb.ca>
2026-09-15 13:49:12 +0000

    First commit the helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2026 Bryson Grant All rights reserved
+// .
+// Created by: Bryson Grant
+// Date: 09 15 2026
+// This propgram prints different greetings
+#include <iostream>
+
+int main() {
+    std::cout << "Helllo" << std::endl;
+    std::cout << "Bonjour" << std::endl;
+}
